203cbb0a133b712305789b86a4c74401b4341ec1
tokarzewski <bartlomiej.tokarzewski@gmail.com>
2022-11-15 21:48:12 +0100

    object definition and calculation are seperated

--- src/example2.py
+++ src/example2.py
@@ -1,13 +1,17 @@
 from duct import *
 
+# define objects
 ducttype1 = ducttype("ductype1", "rigid", "rectangular", 0.00009, None, 1, 1)
 duct1 = duct("duct1", ducttype1, 10, 5)
 
+# calculate object
+ducttype1.calculate()
+duct1.calculate()
 
-for argument in ["area", "hydraulic_diameter"]:
+for argument in ['name', "area", "hydraulic_diameter", 'surface_per_meter']:
     value = str(ducttype1.__getattribute__(argument))
     print(f"{argument}: {value}")
 
-for argument in ["velocity", "pressure_drop_per_meter", "linear_pressure_drop"]:
+for argument in ['name', 'surface_area', 'velocity', 'roughness_correction_factor', 'length', 'pressure_drop_per_meter', 'linear_pressure_drop']:
     value = str(duct1.__getattribute__(argument))
     print(f"{argument}: {value}")
