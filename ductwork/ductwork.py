dd9048916ee4d5ea8519c0acad4f576e8dd557cb
Tokarzewski <bartlomiej.tokarzewski@gmail.com>
2023-01-30 20:14:44 +0100

    refactoring counting and passing flowrate

--- ductwork/ductwork.py
+++ ductwork/ductwork.py
@@ -9,14 +9,7 @@ import networkx as nx
 
 
 def count_connectors(object):
-    if hasattr(object, 'connector4'):
-        return 4
-    elif hasattr(object, 'connector3'):
-        return 3
-    elif hasattr(object, 'connector2'):
-        return 2
-    else:
-        return 1
+    return object.number_of_connectors
 
 
 @dataclass
@@ -29,27 +22,29 @@ class Ductwork:
     def add_object(self, id, object):
         # new instance of class
         object = replace(object)
-        
+
         self.objects.update({id: object})
         count = count_connectors(object)
         if count == 1:
-            self.Graph.add_edge(id, f'{id}.1')
+            self.Graph.add_edge(id, f"{id}.1")
         else:
-            self.Graph.add_edge(id, f'{id}.1')
-            connector_pairs = [(f'{id}.{x+1}', id) for x in range(1, count)]
+            self.Graph.add_edge(id, f"{id}.1")
+            connector_pairs = [(f"{id}.{x+1}", id) for x in range(1, count)]
             for id_x, id in connector_pairs:
                 self.Graph.add_edge(id_x, id)
-    
+
     def set_flowrate_attribute_to_all_nodes(self):
-        nx.set_node_attributes(self.Graph, None, 'flowrate')
-    
+        nx.set_node_attributes(self.Graph, None, "flowrate")
+
     def pass_terminal_flowrate_from_object_to_graph(self):
         for id, object in self.objects.items():
             count = count_connectors(object)
             if count == 1:
-                flowrate = object.connector1.flowrate
+                flowrate = object.connectors.flowrate
+                print(object)
+                print("flowrate:", flowrate)
                 self.Graph.nodes[id]["flowrate"] = flowrate
-                self.Graph.nodes[f'{id}.1']["flowrate"] = flowrate
+                self.Graph.nodes[f"{id}.1"]["flowrate"] = flowrate
 
     def pass_flowrate(self):
 
@@ -74,8 +69,8 @@ class Ductwork:
             W.remove_nodes_from(fittings34)
             for set in nx.weakly_connected_components(W):
                 for fitting in fittings:
-                    if fitting+'.1' in set:
-                        flowrate = G.nodes[fitting+'.1']['flowrate']
+                    if fitting + ".1" in set:
+                        flowrate = G.nodes[fitting + ".1"]["flowrate"]
                         dict1.update({key: flowrate for key in set})
             nx.set_node_attributes(G, dict1, "flowrate")
 
@@ -83,23 +78,23 @@ class Ductwork:
             set1 = set()
             dictionary = dict()
             for fitting in fittings:
-                c2 = G.nodes[fitting+'.2']["flowrate"]
-                c3 = G.nodes[fitting+'.3']["flowrate"]
+                c2 = G.nodes[fitting + ".2"]["flowrate"]
+                c3 = G.nodes[fitting + ".3"]["flowrate"]
                 if None not in (c2, c3):
                     if type(self.objects[fitting]).__name__ == "ThreeWayFitting":
                         flowrate = c2 + c3
                     elif type(self.objects[fitting]).__name__ == "FourWayFitting":
-                        c4 = G.nodes[fitting+'.4']["flowrate"]
+                        c4 = G.nodes[fitting + ".4"]["flowrate"]
                         flowrate = c2 + c3 + c4
                     set1.add(fitting)
-                    dictionary.update({fitting+'.1': flowrate})
+                    dictionary.update({fitting + ".1": flowrate})
                     dictionary.update({fitting: flowrate})
             nx.set_node_attributes(G, dictionary, "flowrate")
             return set1
 
         pass_flowrate_from(air_terminals)
         remaining_fittings = fittings34
-        
+
         fitting_count = len(fittings34)
         while fitting_count > 0:
             calculated_fittings = pass_flowrate_in_fittings(remaining_fittings)
@@ -109,37 +104,32 @@ class Ductwork:
 
         # pass flowrate from nodes to objects
         for id, object in self.objects.items():
-            if type(object).__name__ in ['RigidDuct', 'FlexDuct']:
-                object.flowrate = self.Graph.nodes[id]['flowrate']
-            if type(object).__name__ == 'TwoWayFitting':
-                object.connector1.flowrate = self.Graph.nodes[id+'.1']['flowrate']
-                object.connector2.flowrate = self.Graph.nodes[id+'.2']['flowrate']
-            if type(object).__name__ == "ThreeWayFitting":
-                object.connector1.flowrate = self.Graph.nodes[id+'.1']['flowrate']
-                object.connector2.flowrate = self.Graph.nodes[id+'.2']['flowrate']
-                object.connector3.flowrate = self.Graph.nodes[id+'.3']['flowrate']
-            if type(object).__name__ == "FourWayFitting":
-                object.connector1.flowrate = self.Graph.nodes[id+'.1']['flowrate']
-                object.connector2.flowrate = self.Graph.nodes[id+'.2']['flowrate']
-                object.connector3.flowrate = self.Graph.nodes[id+'.3']['flowrate']
-                object.connector4.flowrate = self.Graph.nodes[id+'.4']['flowrate']
+            if object.number_of_connectors > 1:
+                #print(object.number_of_connectors)
+                #print(object.connectors)
+                for connector in object.connectors:
+                    print(self.Graph.nodes[id + "." + connector.id]["flowrate"])
+                    connector.flowrate = self.Graph.nodes[id + "." + connector.id]["flowrate"]
+            else:
+                connector = object.connectors
+                connector.flowrate = self.Graph.nodes[id + "." + connector.id]["flowrate"]
 
-    def sizing_dimmensions(self):
+    def calculate_dimmensions(self):
         3
 
-    def calculate(self):
+    def calculate_pressure_drops(self):
         # calculate linear pressure drops
         for object in self.objects.values():
-            if type(object).__name__ in ['RigidDuct', 'FlexDuct']:
+            if type(object).__name__ in ["RigidDuct", "FlexDuct"]:
                 object.calculate()
 
         # calculate local pressure drops
         for object in self.objects.values():
-            if type(object).__name__ in [#'OneWayFitting', 
-                                         'TwoWayFitting', 
-                                         'ThreeWayFitting', 
-                                         #'FourWayFitting'
-                                        ]:
+            if type(object).__name__ in [  #'OneWayFitting',
+                "TwoWayFitting",
+                "ThreeWayFitting",
+                #'FourWayFitting'
+            ]:
                 object.calculate()
-        
-    #def pass_pressure_drops_from_objects_to_graph()
+
+    # def pass_pressure_drops_from_objects_to_graph()
